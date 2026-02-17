7340c5228a415a5783c1a70cccddd52691659d64
Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>
2026-02-18 01:27:18 +0530

    Update NotificationMessageServiceImpl.java
    Signed-off-by: Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>

--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/service/impl/NotificationMessageServiceImpl.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/service/impl/NotificationMessageServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
