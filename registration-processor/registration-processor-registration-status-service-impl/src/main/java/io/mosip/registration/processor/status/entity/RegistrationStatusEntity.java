5d70e030a0b99bd72c739927f80345ad7afcf8f6
Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>
2026-02-18 00:23:02 +0530

    Update RegistrationStatusEntity.java
    Signed-off-by: Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>

--- registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/entity/RegistrationStatusEntity.java
+++ registration-processor/registration-processor-registration-status-service-impl/src/main/java/io/mosip/registration/processor/status/entity/RegistrationStatusEntity.java
@@ -140,6 +140,12 @@ public class RegistrationStatusEntity extends BaseRegistrationEntity<BaseRegistr
 	@Column(name = "last_success_stage_name")
 	private String lastSuccessStageName;
 
+	@Column(name = "needs_notification")
+	private Boolean needsNotification;
+	
+	@Column(name = "notification_sent")
+	private Boolean notificationSent;
+
 	/**
 	 * Instantiates a new registration status entity.
 	 */
