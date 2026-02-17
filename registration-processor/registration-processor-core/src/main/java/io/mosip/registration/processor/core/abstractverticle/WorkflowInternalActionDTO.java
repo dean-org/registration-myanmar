6e0bfc95d4539d8f2d409bd0f212a09fb8b00346
Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>
2026-02-18 00:33:15 +0530

    Update WorkflowInternalActionDTO.java
    Signed-off-by: Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>

--- registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/abstractverticle/WorkflowInternalActionDTO.java
+++ registration-processor/registration-processor-core/src/main/java/io/mosip/registration/processor/core/abstractverticle/WorkflowInternalActionDTO.java
@@ -2,6 +2,7 @@ package io.mosip.registration.processor.core.abstractverticle;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -45,6 +46,8 @@ public class WorkflowInternalActionDTO extends MessageDTO implements Serializabl
 
 	/** The workflow instance id */
 	private String workflowInstanceId;
+
+	private Map<String, String> notificationAttributes;
 
 	/*
 	 * (non-Javadoc)
@@ -204,6 +207,14 @@ public class WorkflowInternalActionDTO extends MessageDTO implements Serializabl
 		return workflowInstanceId;
 	}
 
+	public Map<String, String> getNotificationAttributes() {
+		return notificationAttributes;
+	}
+
+	public void setNotificationAttributes(Map<String, String> notificationAttributes) {
+		this.notificationAttributes = notificationAttributes;
+	}
+	
 	/**
 	 * Sets the workflow instance id.
 	 *
